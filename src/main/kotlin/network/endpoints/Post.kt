1333fd6964eefc19dd3bb26c7cfc5432a0afb15b
Jay Williams <course@jaywilliams.me>
2018-07-20 21:04:14 +0000

    Added API endpoints

--- src/main/kotlin/network/endpoints/Post.kt
+++ src/main/kotlin/network/endpoints/Post.kt
@@ -3,7 +3,19 @@
 package com.serebit.diskord.network.endpoints
 
 import com.serebit.diskord.Snowflake
+import com.serebit.diskord.entities.Guild
 import com.serebit.diskord.entities.Message
+import com.serebit.diskord.entities.Role
+import com.serebit.diskord.entities.channels.Channel
+import com.serebit.diskord.entities.channels.DmChannel
 
 internal class CreateMessage(channelId: Snowflake) :
     ApiEndpoint.Post<Message>("/channels/$channelId/messages", setOf(channelId))
+internal object CreateDmChannel :
+    ApiEndpoint.Post<DmChannel>("/users/@me/channels")
+internal object CreateGuild :
+    ApiEndpoint.Post<Guild>("/guilds")
+internal class CreateGuildChannel(guildId: Snowflake) :
+    ApiEndpoint.Post<Channel>("/guilds/$guildId/channels", setOf(guildId))
+internal class CreateGuildRole(guildId: Snowflake) :
+    ApiEndpoint.Post<Role>("/guilds/$guildId/roles", setOf(guildId))
