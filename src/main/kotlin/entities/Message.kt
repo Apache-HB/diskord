90d2eb0868e5d82d854f5ea85c54c089cef696b1
Campbell Jones <git@serebit.com>
2018-08-16 22:31:45 -0400

    Added documentation thanks to ApacheActual

--- src/main/kotlin/entities/Message.kt
+++ src/main/kotlin/entities/Message.kt
@@ -8,24 +8,56 @@ import com.serebit.diskord.internal.network.endpoints.GetChannel
 import com.serebit.diskord.internal.packets.MessagePacket
 import java.time.OffsetDateTime
 
+/**
+ * An object representing a text message sent in a Discord channel.
+ */
 class Message internal constructor(packet: MessagePacket) : Entity {
     override val id: Long = packet.id
+    /** The channel this message was sent from.
+     *
+     * @throws EntityNotFoundException if the channel does not exist.
+     */
     val channel: TextChannel = EntityCache.find(packet.channel_id)
         ?: Requester.requestObject(GetChannel(packet.channel_id)) as? TextChannel
         ?: throw EntityNotFoundException("No channel with ID ${packet.channel_id} found.")
+    /**
+     * The time at which this message was created.
+     */
     val createdAt: OffsetDateTime = OffsetDateTime.parse(packet.timestamp)
+    /**
+     * The message's content as a String, excluding attachments and embeds.
+     */
     var content: String = packet.content
         private set
+    /**
+     * The time at which this message was last edited. If the message has never been edited, this will be null.
+     */
     var editedAt: OffsetDateTime? = packet.edited_timestamp?.let { OffsetDateTime.parse(it) }
         private set
-    var userMentions: List<User> = packet.mentions
+    /**
+     * An unordered list of users that this message contains mentions for.
+     */
+    var userMentions: Set<User> = packet.mentions
         private set
-    var roleMentions: List<Role> = packet.mention_roles
+    /**
+     * An unordered list of roles that this message contains mentions for.
+     */
+    var roleMentions: Set<Role> = packet.mention_roles
         private set
+    /**
+     * Whether or not the message mentions everyone. Only returns true if the user who sent the message has
+     * permission to ping everyone.
+     */
     var mentionsEveryone: Boolean = packet.mention_everyone
         private set
+    /**
+     * Whether or not the message is currently pinned.
+     */
     var isPinned: Boolean = packet.pinned
         private set
+    /**
+     * Whether or not the message was sent with text-to-speech enabled.
+     */
     var isTextToSpeech = packet.tts
         private set
 
--- src/main/kotlin/entities/Role.kt
+++ src/main/kotlin/entities/Role.kt
@@ -1,11 +1,14 @@
 package com.serebit.diskord.entities
 
 import com.serebit.diskord.BitSet
-import com.serebit.diskord.internal.EntityCache
 import com.serebit.diskord.Snowflake
 import com.serebit.diskord.data.Permission
+import com.serebit.diskord.internal.EntityCache
 import java.awt.Color
 
+/**
+ * Represents a role in a Discord server.
+ */
 class Role internal constructor(
     override val id: Snowflake,
     val name: String,
@@ -16,10 +19,25 @@ class Role internal constructor(
     managed: Boolean,
     mentionable: Boolean
 ) : Entity {
-    val color: Color = Color(color)
+    /**
+     * The color assigned to this role as a Java color.
+     */
+    val color = Color(color)
+    /**
+     * The permissions assigned to this role.
+     */
     val permissions = Permission.from(permissions)
+    /**
+     * Whether or not this role appears as its own section in the sidebar.
+     */
     val isHoisted = hoist
+    /**
+     * Whether or not this role is managed by an external source, such as Patreon or a Discord bot.
+     */
     val isManaged = managed
+    /**
+     * Whether or not this role can be mentioned in chat.
+     */
     val isMentionable = mentionable
 
     init {
