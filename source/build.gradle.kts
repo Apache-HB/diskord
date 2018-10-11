242697a23d7a30332e59ab161fbaa1bfe6d4d53f
Campbell Jones <git@serebit.com>
2018-10-11 12:18:35 -0400

    Updated Logkat and formatted buildscript

--- source/build.gradle.kts
+++ source/build.gradle.kts
@@ -10,8 +10,6 @@ plugins {
 group = "com.serebit"
 version = "0.0.0"
 
-tasks.withType<Jar> { baseName = "diskord" }
-
 fun kotlin(module: String) = "org.jetbrains.kotlin:kotlin-$module"
 fun kotlinx(module: String, version: String) = "org.jetbrains.kotlinx:kotlinx-$module:$version"
 fun KotlinDependencyHandler.implementation(group: String, name: String, version: String) =
@@ -22,26 +20,20 @@ kotlin.sourceSets {
         implementation(kotlin("stdlib-common"))
         implementation(kotlin("reflect"))
         implementation(kotlinx("coroutines-core-common", version = "0.30.2-eap13"))
-        implementation(group = "com.serebit", name = "logkat-common", version = "0.4.1-eap13")
+        implementation(group = "com.serebit", name = "logkat-metadata", version = "0.4.2")
         implementation(group = "io.ktor", name = "ktor-client", version = "1.0.0-alpha-1")
     }
-    create("jvmMain") {
-        dependsOn(getByName("commonMain"))
-        dependencies {
-            implementation(kotlin("stdlib-jdk8"))
-            implementation(kotlinx("coroutines-core", version = "0.30.2-eap13"))
-            implementation(group = "com.serebit", name = "logkat-jvm", version = "0.4.1-eap13")
-            implementation(group = "io.ktor", name = "ktor-client-cio", version = "1.0.0-alpha-1")
-            implementation(group = "org.http4k", name = "http4k-client-websocket", version = "3.38.1")
-            implementation(group = "com.fasterxml.jackson.core", name = "jackson-core", version = "2.9.7")
-            implementation(group = "com.fasterxml.jackson.module", name = "jackson-module-kotlin", version = "2.9.7")
-        }
+    create("jvmMain").dependencies {
+        implementation(kotlin("stdlib-jdk8"))
+        implementation(kotlinx("coroutines-core", version = "0.30.2-eap13"))
+        implementation(group = "com.serebit", name = "logkat-jvm", version = "0.4.2")
+        implementation(group = "io.ktor", name = "ktor-client-cio", version = "1.0.0-alpha-1")
+        implementation(group = "org.http4k", name = "http4k-client-websocket", version = "3.38.1")
+        implementation(group = "com.fasterxml.jackson.core", name = "jackson-core", version = "2.9.7")
+        implementation(group = "com.fasterxml.jackson.module", name = "jackson-module-kotlin", version = "2.9.7")
     }
-    create("jvmTest") {
-        dependsOn(getByName("commonTest"))
-        dependencies {
-            implementation(group = "io.kotlintest", name = "kotlintest-runner-jvm", version = "3.1.10")
-        }
+    create("jvmTest").dependencies {
+        implementation(group = "io.kotlintest", name = "kotlintest-runner-jvm", version = "3.1.10")
     }
 }
 
