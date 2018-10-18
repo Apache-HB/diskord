3715a69f389770fd00b2612e0d285cdff3d4cfe8
Campbell Jones <git@serebit.com>
2018-10-18 16:03:39 -0400

    Updated Gradle configuration

--- source/build.gradle.kts
+++ source/build.gradle.kts
@@ -2,7 +2,7 @@ import com.jfrog.bintray.gradle.BintrayExtension
 import org.jetbrains.kotlin.gradle.plugin.KotlinDependencyHandler
 
 plugins {
-    kotlin("multiplatform") version "1.3.0-rc-146"
+    kotlin("multiplatform") version "1.3.0-rc-190"
     id("com.jfrog.bintray") version "1.8.4"
     `maven-publish`
 }
@@ -18,22 +18,27 @@ fun KotlinDependencyHandler.implementation(group: String, name: String, version:
 kotlin.sourceSets {
     getByName("commonMain").dependencies {
         implementation(kotlin("stdlib-common"))
-        implementation(kotlin("reflect"))
         implementation(kotlinx("coroutines-core-common", version = "0.30.2-eap13"))
         implementation(group = "com.serebit", name = "logkat-metadata", version = "0.4.2")
-        implementation(group = "io.ktor", name = "ktor-client", version = "1.0.0-alpha-3")
+        implementation(group = "io.ktor", name = "ktor-client", version = "1.0.0-beta-1")
     }
-    create("jvmMain").dependencies {
-        implementation(kotlin("stdlib-jdk8"))
-        implementation(kotlinx("coroutines-core", version = "0.30.2-eap13"))
-        implementation(group = "com.serebit", name = "logkat-jvm", version = "0.4.2")
-        implementation(group = "io.ktor", name = "ktor-client-cio", version = "1.0.0-alpha-3")
-        implementation(group = "org.http4k", name = "http4k-client-websocket", version = "3.39.1")
-        implementation(group = "com.fasterxml.jackson.core", name = "jackson-core", version = "2.9.7")
-        implementation(group = "com.fasterxml.jackson.module", name = "jackson-module-kotlin", version = "2.9.7")
+    create("jvmMain") {
+        dependsOn(getByName("commonMain"))
+        dependencies {
+            implementation(kotlin("stdlib-jdk8"))
+            implementation(kotlinx("coroutines-core", version = "0.30.2-eap13"))
+            implementation(group = "com.serebit", name = "logkat-jvm", version = "0.4.2")
+            implementation(group = "io.ktor", name = "ktor-client-cio", version = "1.0.0-beta-1")
+            implementation(group = "org.http4k", name = "http4k-client-websocket", version = "3.39.2")
+            implementation(group = "com.fasterxml.jackson.core", name = "jackson-core", version = "2.9.7")
+            implementation(group = "com.fasterxml.jackson.module", name = "jackson-module-kotlin", version = "2.9.7")
+        }
     }
-    create("jvmTest").dependencies {
-        implementation(group = "io.kotlintest", name = "kotlintest-runner-jvm", version = "3.1.10")
+    create("jvmTest") {
+        dependsOn(getByName("commonTest"))
+        dependencies {
+            implementation(group = "io.kotlintest", name = "kotlintest-runner-jvm", version = "3.1.10")
+        }
     }
 }
 
@@ -44,7 +49,7 @@ bintray {
     key = System.getenv("BINTRAY_KEY")
     setPublications("jvm")
     pkg(delegateClosureOf<BintrayExtension.PackageConfig> {
-        repo = "test"
+        repo = "public"
         name = rootProject.name
         version.name = project.version.toString()
     })
