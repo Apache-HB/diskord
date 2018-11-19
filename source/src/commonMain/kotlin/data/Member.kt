230c144ac8a93fad0231c98aad7d164933eb8d7f
Campbell Jones <git@serebit.com>
2018-11-18 20:39:03 -0500

    Fix reference to old architecture in Member's usage of roles

--- source/src/commonMain/kotlin/data/Member.kt
+++ source/src/commonMain/kotlin/data/Member.kt
@@ -2,16 +2,13 @@ package com.serebit.diskord.data
 
 import com.serebit.diskord.entities.Role
 import com.serebit.diskord.entities.User
-import com.serebit.diskord.internal.EntityCache
 import com.serebit.diskord.internal.cache
 import com.serebit.diskord.internal.packets.MemberPacket
 
 class Member internal constructor(packet: MemberPacket) {
     val user: User = User(packet.user.cache().id)
     val nickname: String? = packet.nick
-    val roles: List<Role> by lazy {
-        packet.roles.asSequence().map { EntityCache.findId<Role>(it) }.requireNoNulls().toList()
-    }
+    val roles: List<Role> = packet.roles.map(::Role)
     val joinedAt: DateTime = DateTime.fromIsoTimestamp(packet.joined_at)
     val isDeafened: Boolean = packet.deaf
     val isMuted: Boolean = packet.mute
