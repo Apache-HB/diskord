0cdb81a549bfaa1aa986b7671eae8172a81a9fb0
Campbell Jones <git@serebit.com>
2018-11-19 15:28:03 -0500

    Added endpoints for other types of channels

--- source/src/commonMain/kotlin/internal/network/endpoints/ChannelEndpoints.kt
+++ source/src/commonMain/kotlin/internal/network/endpoints/ChannelEndpoints.kt
@@ -1,9 +1,12 @@
 package com.serebit.diskord.internal.network.endpoints
 
+import com.serebit.diskord.internal.packets.ChannelCategoryPacket
 import com.serebit.diskord.internal.packets.ChannelPacket
 import com.serebit.diskord.internal.packets.DmChannelPacket
 import com.serebit.diskord.internal.packets.GroupDmChannelPacket
 import com.serebit.diskord.internal.packets.GuildChannelPacket
+import com.serebit.diskord.internal.packets.GuildTextChannelPacket
+import com.serebit.diskord.internal.packets.GuildVoiceChannelPacket
 import io.ktor.http.HttpMethod
 
 internal class GetChannel(channelId: Long) : Endpoint.Object<ChannelPacket>(
@@ -18,6 +21,21 @@ internal class GetDmChannel(channelId: Long) : Endpoint.Object<DmChannelPacket>(
 
 internal class GetGroupDmChannel(channelId: Long) : Endpoint.Object<GroupDmChannelPacket>(
     HttpMethod.Get, "channels/$channelId", GroupDmChannelPacket.serializer(),
+    channelId
+)
+
+internal class GetGuildTextChannel(channelId: Long) : Endpoint.Object<GuildTextChannelPacket>(
+    HttpMethod.Get, "channels/$channelId", GuildTextChannelPacket.serializer(),
+    channelId
+)
+
+internal class GetGuildVoiceChannel(channelId: Long) : Endpoint.Object<GuildVoiceChannelPacket>(
+    HttpMethod.Get, "channels/$channelId", GuildVoiceChannelPacket.serializer(),
+    channelId
+)
+
+internal class GetChannelCategory(channelId: Long) : Endpoint.Object<ChannelCategoryPacket>(
+    HttpMethod.Get, "channels/$channelId", ChannelCategoryPacket.serializer(),
     channelId
 )
 
