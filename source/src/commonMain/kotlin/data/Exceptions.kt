0d19a86c50d3a42c3c45718126e2c0683c7a0897
Campbell Jones <git@serebit.com>
2018-11-28 15:34:25 -0500

    Added UnknownEntityTypeException and updated others

--- source/src/commonMain/kotlin/data/Exceptions.kt
+++ source/src/commonMain/kotlin/data/Exceptions.kt
@@ -2,6 +2,8 @@ package com.serebit.diskord.data
 
 class EntityNotFoundException(message: String) : RuntimeException(message)
 
-class UnknownTypeCodeException(message: String) : RuntimeException(message)
+class UnknownTypeCodeException(message: String) : IllegalArgumentException(message)
 
-class UnknownOpcodeException(message: String) : RuntimeException(message)
+class UnknownEntityTypeException(message: String) : IllegalArgumentException(message)
+
+class UnknownOpcodeException(message: String) : IllegalArgumentException(message)
