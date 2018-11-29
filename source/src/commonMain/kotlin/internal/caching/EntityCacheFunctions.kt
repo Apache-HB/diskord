c59936948712591e613a8f35227dd77525bbcd4b
Campbell Jones <git@serebit.com>
2018-11-29 15:11:26 -0500

    Renamed put and putAll extensions to add and addAll to avoid conflicts

--- source/src/commonMain/kotlin/internal/caching/EntityCacheFunctions.kt
+++ source/src/commonMain/kotlin/internal/caching/EntityCacheFunctions.kt
@@ -2,9 +2,9 @@ package com.serebit.diskord.internal.caching
 
 import com.serebit.diskord.internal.entitydata.EntityData
 
-internal fun <T : EntityData> MutableMap<Long, T>.put(data: T) = put(data.id, data)
+internal fun <T : EntityData> MutableMap<Long, T>.add(data: T) = put(data.id, data)
 
-internal fun <T : EntityData> MutableMap<Long, T>.putAll(elements: Iterable<T>) = putAll(elements.associateBy { it.id })
+internal fun <T : EntityData> MutableMap<Long, T>.addAll(elements: Iterable<T>) = putAll(elements.associateBy { it.id })
 
 internal operator fun <T : EntityData> MutableMap<Long, T>.plusAssign(data: T) = plusAssign(data.id to data)
 
