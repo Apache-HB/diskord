24012d950e38f33aca7e32d480be9026e920ec58
Campbell Jones <git@serebit.com>
2018-11-29 16:55:44 -0500

    Removed logLevel setting from Ping sample

--- samples/ping/src/main/kotlin/Ping.kt
+++ samples/ping/src/main/kotlin/Ping.kt
@@ -2,14 +2,11 @@ package samples
 
 import com.serebit.diskord.bot
 import com.serebit.diskord.events.MessageCreatedEvent
-import com.serebit.logkat.LogLevel
 
 fun main(args: Array<String>) {
     val token = args.getOrNull(0) ?: error("No token passed.")
 
     bot(token) {
-        logLevel = LogLevel.TRACE
-
         onEvent { evt: MessageCreatedEvent ->
             if (evt.message.content == "!ping") evt.message.reply("Pong.")
         }
