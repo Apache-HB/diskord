49e5e29f84f557666ed803e89c33781a269a4b09
Campbell Jones <git@serebit.com>
2018-11-29 17:44:02 -0500

    Added more static colors to Color

--- source/src/commonMain/kotlin/data/Color.kt
+++ source/src/commonMain/kotlin/data/Color.kt
@@ -4,27 +4,18 @@ package com.serebit.diskord.data
  * Represents a color in the sRGB color space, with black being 0x000000 and white being 0xFFFFFF. The first 2 places
  * represent red, the second two places represent green, and the last two places represent blue.
  *
- * @constructor Composes a new [Color] from the composite of the values of the three color channels (red, green, and
- * blue).
+ * @constructor Composes a new [Color] from the individual values of the three color channels.
+ *
+ * @property red The red bits in the color, ranging from 0 to 255, or hex 0x00 to 0xFF.
+ * @property green The green bits in the color, ranging from 0 to 255, or hex 0x00 to 0xFF.
+ * @property blue The blue bits in the color, ranging from 0 to 255, or hex 0x00 to 0xFF.
  */
-data class Color(val rgb: Int) {
+data class Color(val red: Int, val green: Int, val blue: Int) {
     /**
-     * The red bits in the color, ranging from 0 to 255, or hex 0x00 to 0xFF.
+     * Composes a new [Color] from the composite of the values of the three color channels (red, green, and
+     * blue).
      */
-    val red: Int = rgb shr 16 and 0xFF
-    /**
-     * The green bits in the color, ranging from 0 to 255, or hex 0x00 to 0xFF.
-     */
-    val green: Int = rgb shr 8 and 0xFF
-    /**
-     * The blue bits in the color, ranging from 0 to 255, or hex 0x00 to 0xFF.
-     */
-    val blue: Int = rgb and 0xFF
-
-    /**
-     * Composes a new [Color] from the individual values of the three color channels.
-     */
-    constructor(red: Int, green: Int, blue: Int) : this(red shl 16 + green shl 8 + blue)
+    constructor(rgb: Int) : this(rgb shr 16 and 0xFF, rgb shr 8 and 0xFF, rgb and 0xFF)
 
     companion object {
         val black = Color(0x000000)
@@ -33,10 +24,15 @@ data class Color(val rgb: Int) {
         val lightGrey = Color(0xC0C0C0)
         val white = Color(0xFFFFFF)
         val red = Color(0xFF0000)
+        val orange = Color(0xFF8000)
         val yellow = Color(0xFFFF00)
+        val lime = Color(0x80FF00)
         val green = Color(0x00FF00)
+        val marine = Color(0x00FF89)
         val cyan = Color(0x00FFFF)
+        val sky = Color(0x0080FF)
         val blue = Color(0x0000FF)
+        val purple = Color(0x8000FF)
         val magenta = Color(0xFF00FF)
     }
 }
