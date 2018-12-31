b75ef21eafbedf69d3761851228722b55b0f61d9
Campbell Jones <git@serebit.com>
2018-12-31 13:43:12 -0700

    Removed exitProcess function as it is no longer used

--- core/src/commonMain/kotlin/internal/System.kt
+++ core/src/commonMain/kotlin/internal/System.kt
@@ -2,6 +2,4 @@ package com.serebit.strife.internal
 
 internal expect val osName: String
 
-internal expect fun exitProcess(code: Int): Nothing
-
 internal expect fun onProcessExit(callback: suspend () -> Unit)
--- core/src/jvmMain/kotlin/internal/System.kt
+++ core/src/jvmMain/kotlin/internal/System.kt
@@ -1,11 +1,8 @@
 package com.serebit.strife.internal
 
 import kotlin.concurrent.thread
-import kotlin.system.exitProcess
 
 internal actual val osName: String get() = System.getProperty("os.name")
-
-internal actual fun exitProcess(code: Int): Nothing = exitProcess(code)
 
 internal actual fun onProcessExit(callback: suspend () -> Unit) =
     Runtime.getRuntime().addShutdownHook(thread(false) {
