f28a4c9470417bf4abbaacf1167af8bb1d74dbd9
Campbell Jones <git@serebit.com>
2019-02-07 21:40:32 -0500

    Formatting of Payloads file and classes

--- core/src/commonMain/kotlin/internal/Payloads.kt
+++ core/src/commonMain/kotlin/internal/Payloads.kt
@@ -3,54 +3,44 @@ package com.serebit.strife.internal
 import com.serebit.strife.Context
 import com.serebit.strife.data.UnknownOpcodeException
 import com.serebit.strife.events.Event
-import com.serebit.strife.internal.dispatches.ChannelCreate
-import com.serebit.strife.internal.dispatches.ChannelDelete
-import com.serebit.strife.internal.dispatches.ChannelPinsUpdate
-import com.serebit.strife.internal.dispatches.ChannelUpdate
-import com.serebit.strife.internal.dispatches.GuildCreate
-import com.serebit.strife.internal.dispatches.GuildDelete
-import com.serebit.strife.internal.dispatches.GuildUpdate
-import com.serebit.strife.internal.dispatches.MessageCreate
-import com.serebit.strife.internal.dispatches.MessageDelete
-import com.serebit.strife.internal.dispatches.MessageUpdate
-import com.serebit.strife.internal.dispatches.Ready
-import com.serebit.strife.internal.dispatches.TypingStart
-import com.serebit.strife.internal.dispatches.Unknown
+import com.serebit.strife.internal.dispatches.*
 import kotlinx.serialization.KSerializer
 import kotlinx.serialization.Serializable
 import kotlinx.serialization.json.Json
+import kotlinx.serialization.json.content
+import kotlinx.serialization.json.int
+
+private object Opcodes {
+    const val DISPATCH = 0
+    const val HEARTBEAT = 1
+    const val IDENTIFY = 2
+    const val RESUME = 6
+    const val HELLO = 10
+    const val HEARTBEAT_ACK = 11
+}
 
 @Serializable
 internal sealed class Payload(val op: Int) {
-    @Serializable
-    private data class BasicPayload(val op: Int)
-
     companion object {
-        fun from(json: String): Payload? =
-            when (val opcode = Json.nonstrict.parse(BasicPayload.serializer(), json).op) {
-                DispatchPayload.opcode -> DispatchPayload.from(json)
-                HelloPayload.opcode -> Json.nonstrict.parse(HelloPayload.serializer(), json)
-                IdentifyPayload.opcode -> Json.nonstrict.parse(IdentifyPayload.serializer(), json)
-                ResumePayload.opcode -> Json.nonstrict.parse(ResumePayload.serializer(), json)
-                HeartbeatPayload.opcode -> Json.nonstrict.parse(HeartbeatPayload.serializer(), json)
-                HeartbeatAckPayload.opcode -> Json.nonstrict.parse(HeartbeatAckPayload.serializer(), json)
-                else -> throw UnknownOpcodeException("Received a payload with an unknown opcode of $opcode.")
-            }
+        fun from(json: String) = when (val opcode = Json.nonstrict.parseJson(json).jsonObject["op"].int) {
+            Opcodes.DISPATCH -> DispatchPayload.from(json)
+            Opcodes.HELLO -> Json.nonstrict.parse(HelloPayload.serializer(), json)
+            Opcodes.IDENTIFY -> Json.nonstrict.parse(IdentifyPayload.serializer(), json)
+            Opcodes.RESUME -> Json.nonstrict.parse(ResumePayload.serializer(), json)
+            Opcodes.HEARTBEAT -> Json.nonstrict.parse(HeartbeatPayload.serializer(), json)
+            Opcodes.HEARTBEAT_ACK -> Json.nonstrict.parse(HeartbeatAckPayload.serializer(), json)
+            else -> throw UnknownOpcodeException("Received a payload with an unknown opcode of $opcode.")
+        }
     }
 }
 
-internal abstract class DispatchPayload : Payload(opcode) {
+internal abstract class DispatchPayload : Payload(Opcodes.DISPATCH) {
     abstract val d: Any
     abstract val s: Int
 
     abstract suspend fun asEvent(context: Context): Event?
 
-    @Serializable
-    private data class BasicDispatch(val t: String)
-
     companion object {
-        const val opcode = 0
-
         private val dispatchTypeAssociations: Map<String, KSerializer<out DispatchPayload>> = mapOf(
             "READY" to Ready.serializer(),
             "GUILD_CREATE" to GuildCreate.serializer(),
@@ -67,54 +57,34 @@ internal abstract class DispatchPayload : Payload(opcode) {
         )
 
         fun from(json: String): DispatchPayload {
-            val type = Json.nonstrict.parse(BasicDispatch.serializer(), json).t
+            val type = Json.nonstrict.parseJson(json).jsonObject["t"].content
             return Json.nonstrict.parse(dispatchTypeAssociations[type] ?: Unknown.serializer(), json)
         }
     }
 }
 
 @Serializable
-internal class HeartbeatAckPayload : Payload(opcode) {
-    companion object {
-        const val opcode = 11
-    }
-}
+internal class HeartbeatAckPayload : Payload(Opcodes.HEARTBEAT_ACK)
 
 @Serializable
-internal data class HeartbeatPayload(val d: Int?) : Payload(opcode) {
-    companion object {
-        const val opcode = 1
-    }
-}
+internal data class HeartbeatPayload(val d: Int?) : Payload(Opcodes.HEARTBEAT)
 
 @Serializable
-internal data class HelloPayload(val d: Data) : Payload(opcode) {
+internal data class HelloPayload(val d: Data) : Payload(Opcodes.HELLO) {
     @Serializable
     data class Data(val heartbeat_interval: Long, val _trace: List<String>)
-
-    companion object {
-        const val opcode = 10
-    }
 }
 
 @Serializable
-internal data class IdentifyPayload(val d: Data) : Payload(opcode) {
+internal data class IdentifyPayload(val d: Data) : Payload(Opcodes.IDENTIFY) {
     @Serializable
     data class Data(val token: String, val properties: Map<String, String>)
-
-    companion object {
-        const val opcode = 2
-    }
 }
 
 @Serializable
-internal data class ResumePayload(val d: Data) : Payload(opcode) {
+internal data class ResumePayload(val d: Data) : Payload(Opcodes.RESUME) {
     @Serializable
     data class Data(val token: String, val session_id: String, val seq: Int)
 
     constructor(token: String, session_id: String, seq: Int) : this(Data(token, session_id, seq))
-
-    companion object {
-        const val opcode = 6
-    }
 }
