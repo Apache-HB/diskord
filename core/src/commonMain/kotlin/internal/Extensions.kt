b1f7c03aff2dfbcb0eca799f7d880f7c29fd3dcc
Campbell Jones <git@serebit.com>
2019-02-09 17:38:31 -0500

    Merge CoroutineExtensions and Extensions files

--- core/src/commonMain/kotlin/internal/Extensions.kt
+++ core/src/commonMain/kotlin/internal/Extensions.kt
@@ -1,5 +1,9 @@
 package com.serebit.strife.internal
 
 import com.soywiz.klock.DateFormat
+import kotlinx.coroutines.CoroutineScope
 
 internal val DateFormat.Companion.ISO_FORMAT get() = DateFormat("yyyy-MM-dd'T'HH:mm:ss.SSSXXX")
+
+// runBlocking doesn't exist in the common coroutines library because of JS, so create it here for use in common
+expect fun <T> runBlocking(block: suspend CoroutineScope.() -> T): T
