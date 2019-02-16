6c24af0f651b52241dabe7b891ce275281544910
Campbell Jones <git@serebit.com>
2019-02-15 22:07:13 -0500

    Make runBlocking internal

--- core/src/commonMain/kotlin/internal/Extensions.kt
+++ core/src/commonMain/kotlin/internal/Extensions.kt
@@ -6,4 +6,4 @@ import kotlinx.coroutines.CoroutineScope
 internal val DateFormat.Companion.ISO_FORMAT get() = DateFormat("yyyy-MM-dd'T'HH:mm:ss.SSSXXX")
 
 // runBlocking doesn't exist in the common coroutines library because of JS, so create it here for use in common
-expect fun <T> runBlocking(block: suspend CoroutineScope.() -> T): T
+internal expect fun <T> runBlocking(block: suspend CoroutineScope.() -> T): T
