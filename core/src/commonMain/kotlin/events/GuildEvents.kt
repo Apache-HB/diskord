aecf98f72f2d5ff472ba80f09ef3f15a0658b4a1
Campbell Jones <git@serebit.com>
2019-03-11 20:44:22 -0400

    Migrated guild event build logic to dispatches

--- core/src/commonMain/kotlin/events/GuildEvents.kt
+++ core/src/commonMain/kotlin/events/GuildEvents.kt
@@ -4,13 +4,6 @@ import com.serebit.strife.Context
 import com.serebit.strife.data.Member
 import com.serebit.strife.entities.Guild
 import com.serebit.strife.entities.User
-import com.serebit.strife.internal.dispatches.GuildBanAdd
-import com.serebit.strife.internal.dispatches.GuildBanRemove
-import com.serebit.strife.internal.dispatches.GuildMemberRemove
-import com.serebit.strife.internal.packets.GuildCreatePacket
-import com.serebit.strife.internal.packets.GuildUpdatePacket
-import com.serebit.strife.internal.packets.MemberPacket
-import com.serebit.strife.internal.packets.UnavailableGuildPacket
 
 interface GuildEvent : Event {
     val guild: Guild
@@ -24,51 +17,41 @@ interface GuildEvent : Event {
  * - When a Guild becomes available again to the client.
  * - When the current user joins a new Guild.
  */
-class GuildCreateEvent internal constructor(override val context: Context, packet: GuildCreatePacket) : GuildEvent {
-    override val guild = context.cache.pushGuildData(packet).toEntity()
-}
+class GuildCreateEvent internal constructor(override val context: Context, override val guild: Guild) : GuildEvent
 
 /** Sent when a guild is updated. (TODO better docs. Thanks, Discord...) */
-class GuildUpdateEvent internal constructor(override val context: Context, packet: GuildUpdatePacket) : GuildEvent {
-    override val guild = context.cache.pullGuildData(packet).toEntity()
-}
+class GuildUpdateEvent internal constructor(override val context: Context, override val guild: Guild) : GuildEvent
 
 /**
  * Sent when a guild becomes unavailable during a guild outage, or when the client leaves or is removed from a guild.
  */
-class GuildDeleteEvent internal constructor(override val context: Context, packet: UnavailableGuildPacket) : Event {
-    val guildID: Long = packet.id
-    /** `true` if the bot-client was kicked from this [guild][com.serebit.strife.entities.Guild]. */
-    val wasKicked: Boolean = packet.unavailable == null
+class GuildDeleteEvent internal constructor(
+    override val context: Context,
+    val guildID: Long,
+    val wasKicked: Boolean
+) : Event
 
-    init {
-        context.cache.decache(packet.id)
-    }
-}
-
-class GuildBanAddEvent internal constructor(override val context: Context, packet: GuildBanAdd.Data) : GuildEvent {
-    override val guild = context.cache.getGuildData(packet.guild_id)!!.toEntity()
-    val user: User = context.cache.pullUserData(packet.user).toEntity()
-}
+class GuildBanAddEvent internal constructor(
+    override val context: Context,
+    override val guild: Guild,
+    val user: User
+) : GuildEvent
 
 class GuildBanRemoveEvent internal constructor(
-    override val context: Context, packet: GuildBanRemove.Data
-) : GuildEvent {
-    override val guild = context.cache.getGuildData(packet.guild_id)!!.toEntity()
-    val user: User = context.cache.pullUserData(packet.user).toEntity()
-}
+    override val context: Context,
+    override val guild: Guild,
+    val user: User
+) : GuildEvent
 
-class GuildMemberJoinEvent internal constructor(override val context: Context, packet: MemberPacket) : GuildEvent {
-    private val guildData = context.cache.getGuildData(packet.guild_id!!)!!
-    val member: Member = Member(packet, guildData, context)
-    override val guild = guildData.also { it.members += member }.toEntity()
-}
+
+class GuildMemberJoinEvent internal constructor(
+    override val context: Context,
+    override val guild: Guild,
+    val member: Member
+) : GuildEvent
 
 class GuildMemberLeaveEvent internal constructor(
-    override val context: Context, packet: GuildMemberRemove.Data
-) : GuildEvent {
-    override val guild = context.cache.getGuildData(packet.guild_id)!!.also { data ->
-        data.members.removeAll { it.user.id == user.id }
-    }.toEntity()
-    val user: User = context.cache.pullUserData(packet.user).toEntity()
-}
+    override val context: Context,
+    override val guild: Guild,
+    val user: User
+) : GuildEvent
--- core/src/commonMain/kotlin/internal/dispatches/GuildDispatches.kt
+++ core/src/commonMain/kotlin/internal/dispatches/GuildDispatches.kt
@@ -1,6 +1,7 @@
 package com.serebit.strife.internal.dispatches
 
 import com.serebit.strife.Context
+import com.serebit.strife.data.Member
 import com.serebit.strife.events.*
 import com.serebit.strife.internal.DispatchPayload
 import com.serebit.strife.internal.packets.*
@@ -8,22 +9,33 @@ import kotlinx.serialization.Serializable
 
 @Serializable
 internal class GuildCreate(override val s: Int, override val d: GuildCreatePacket) : DispatchPayload() {
-    override suspend fun asEvent(context: Context) = GuildCreateEvent(context, d)
+    override suspend fun asEvent(context: Context) =
+        GuildCreateEvent(context, guild = context.cache.pushGuildData(d).toEntity())
 }
 
 @Serializable
 internal class GuildUpdate(override val s: Int, override val d: GuildUpdatePacket) : DispatchPayload() {
-    override suspend fun asEvent(context: Context) = GuildUpdateEvent(context, d)
+    override suspend fun asEvent(context: Context) =
+        GuildUpdateEvent(context, guild = context.cache.pullGuildData(d).toEntity())
 }
 
 @Serializable
 internal class GuildDelete(override val s: Int, override val d: UnavailableGuildPacket) : DispatchPayload() {
-    override suspend fun asEvent(context: Context) = GuildDeleteEvent(context, d)
+    override suspend fun asEvent(context: Context): GuildDeleteEvent {
+        context.cache.decache(d.id)
+
+        return GuildDeleteEvent(context, guildID = d.id, wasKicked = d.unavailable == null)
+    }
 }
 
 @Serializable
 internal class GuildBanAdd(override val s: Int, override val d: Data) : DispatchPayload() {
-    override suspend fun asEvent(context: Context) = GuildBanAddEvent(context, d)
+    override suspend fun asEvent(context: Context): GuildBanAddEvent? {
+        val guild = context.cache.getGuildData(d.guild_id)?.toEntity() ?: return null
+        val user = context.cache.pullUserData(d.user).toEntity()
+
+        return GuildBanAddEvent(context, guild, user)
+    }
 
     @Serializable
     data class Data(val guild_id: Long, val user: UserPacket)
@@ -31,7 +43,12 @@ internal class GuildBanAdd(override val s: Int, override val d: Data) : Dispatch
 
 @Serializable
 internal class GuildBanRemove(override val s: Int, override val d: Data) : DispatchPayload() {
-    override suspend fun asEvent(context: Context) = GuildBanRemoveEvent(context, d)
+    override suspend fun asEvent(context: Context): GuildBanRemoveEvent? {
+        val guild = context.cache.getGuildData(d.guild_id)?.toEntity() ?: return null
+        val user = context.cache.pullUserData(d.user).toEntity()
+
+        return GuildBanRemoveEvent(context, guild, user)
+    }
 
     @Serializable
     data class Data(val guild_id: Long, val user: UserPacket)
@@ -39,12 +56,23 @@ internal class GuildBanRemove(override val s: Int, override val d: Data) : Dispa
 
 @Serializable
 internal class GuildMemberAdd(override val s: Int, override val d: MemberPacket) : DispatchPayload() {
-    override suspend fun asEvent(context: Context) = GuildMemberJoinEvent(context, d)
+    override suspend fun asEvent(context: Context): GuildMemberJoinEvent? {
+        val guildData = d.guild_id?.let { context.cache.getGuildData(d.guild_id) } ?: return null
+        val member = Member(d, guildData, context)
+
+        return GuildMemberJoinEvent(context, guildData.toEntity(), member)
+    }
 }
 
 @Serializable
 internal class GuildMemberRemove(override val s: Int, override val d: Data) : DispatchPayload() {
-    override suspend fun asEvent(context: Context) = GuildMemberLeaveEvent(context, d)
+    override suspend fun asEvent(context: Context): GuildMemberLeaveEvent? {
+        val guildData = context.cache.getGuildData(d.guild_id) ?: return null
+        val user = context.cache.pullUserData(d.user).toEntity()
+        guildData.members.removeAll { it.user.id == user.id }
+
+        return GuildMemberLeaveEvent(context, guildData.toEntity(), user)
+    }
 
     @Serializable
     data class Data(val guild_id: Long, val user: UserPacket)
