282b46f5e759a1b1f3f1a2a4c4cb4ed0573095c9
Campbell Jones <git@serebit.com>
2019-03-14 17:32:05 -0400

    Migrated the rest of the event build logic to dispatches

--- core/src/commonMain/kotlin/events/ChannelEvents.kt
+++ core/src/commonMain/kotlin/events/ChannelEvents.kt
@@ -2,42 +2,33 @@ package com.serebit.strife.events
 
 import com.serebit.strife.Context
 import com.serebit.strife.entities.Channel
-import com.serebit.strife.internal.ISO_WITH_MS
-import com.serebit.strife.internal.dispatches.ChannelPinsUpdate
-import com.serebit.strife.internal.dispatches.TypingStart
-import com.serebit.strife.internal.packets.ChannelPacket
-import com.soywiz.klock.DateFormat
+import com.serebit.strife.entities.User
 import com.soywiz.klock.DateTime
-import com.soywiz.klock.parse
 
 interface ChannelEvent : Event {
     val channel: Channel
 }
 
-class ChannelCreateEvent internal constructor(override val context: Context, packet: ChannelPacket) : ChannelEvent {
-    override val channel = context.cache.pushChannelData(packet).toEntity()
-}
+class ChannelCreateEvent internal constructor(
+    override val context: Context,
+    override val channel: Channel
+) : ChannelEvent
 
-class ChannelUpdateEvent internal constructor(override val context: Context, packet: ChannelPacket) : ChannelEvent {
-    override val channel = context.cache.pullChannelData(packet).toEntity()
-}
+class ChannelUpdateEvent internal constructor(
+    override val context: Context,
+    override val channel: Channel
+) : ChannelEvent
 
-class ChannelDeleteEvent internal constructor(override val context: Context, packet: ChannelPacket) : Event {
-    val channelID = packet.apply { context.cache.decache(id) }.id
-}
+class ChannelDeleteEvent internal constructor(override val context: Context, val channelID: Long) : Event
 
 class ChannelPinsUpdateEvent internal constructor(
-    override val context: Context, data: ChannelPinsUpdate.Data
-) : ChannelEvent {
-    override val channel = context.cache.getTextChannelData(data.channel_id)!!.also {
-        it.lastPinTime = data.last_pin_timestamp?.let { time -> DateFormat.ISO_WITH_MS.parse(time) }
-    }.toEntity()
-}
+    override val context: Context,
+    override val channel: Channel
+) : ChannelEvent
 
 class TypingStartEvent internal constructor(
-    override val context: Context, data: TypingStart.Data
-) : ChannelEvent {
-    val user by lazy { context.cache.getUserData(data.user_id)!!.toEntity() }
-    override val channel by lazy { context.cache.getTextChannelData(data.channel_id)!!.toEntity() }
-    val timestamp = DateTime(data.timestamp)
-}
+    override val context: Context,
+    override val channel: Channel,
+    val user: User,
+    val timestamp: DateTime
+) : ChannelEvent
