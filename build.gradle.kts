91ee06e9e516f59cb8e42ce76e84671a44f4a560
Campbell Jones <git@serebit.com>
2019-04-02 21:17:49 -0400

    Enable build scan on CI builds and parallel Kotlin task execution

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,10 +5,17 @@ import com.serebit.strife.gradle.soywiz
 
 plugins {
     id("com.github.ben-manes.versions") version "0.21.0"
+    id("com.gradle.build-scan") version "2.2.1"
     kotlin("multiplatform") version "1.3.30-eap-125" apply false
     id("org.jetbrains.dokka") version "0.9.18" apply false
     id("kotlinx-serialization") version "1.3.30-eap-125" apply false
     id("com.jfrog.bintray") version "1.8.4" apply false
+}
+
+buildScan {
+    termsOfServiceAgree = "yes"
+
+    publishAlwaysIf(System.getenv("PUBLISH_BUILD_SCAN") == "true")
 }
 
 subprojects {
