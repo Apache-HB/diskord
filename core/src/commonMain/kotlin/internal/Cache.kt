0ad52f70ac518e88b1a2fd960a0e37b7d4a7c734
LegendOfLelouch <LegendOfLelouch@gmail.com>
2019-04-30 08:42:34 +0300

    Add `WeakReference`. Everything is broken

--- core/src/commonMain/kotlin/internal/Cache.kt
+++ core/src/commonMain/kotlin/internal/Cache.kt
@@ -178,50 +178,6 @@ internal class LruCache<K, V>(
     }
 }
 
-internal expect class WeakCache<K, V>() : MutableMap<K, V>
-
-internal class CombinationCache<K, V> : MutableMap<K, V> {
-    private val lruCache = LruCache<K, V>()
-    private val weakCache = WeakCache<K, V>()
-
-    override val size: Int get() = lruCache.size + weakCache.size
-
-    override fun containsKey(key: K): Boolean {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-    }
-
-    override fun containsValue(value: V): Boolean {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-    }
-
-    override fun get(key: K): V? {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-    }
-
-    override fun isEmpty(): Boolean {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-    }
-
-    override val entries: MutableSet<MutableMap.MutableEntry<K, V>>
-        get() = TODO("not implemented") //To change initializer of created properties use File | Settings | File Templates.
-    override val keys: MutableSet<K>
-        get() = TODO("not implemented") //To change initializer of created properties use File | Settings | File Templates.
-    override val values: MutableCollection<V>
-        get() = TODO("not implemented") //To change initializer of created properties use File | Settings | File Templates.
-
-    override fun clear() {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-    }
-
-    override fun put(key: K, value: V): V? {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-    }
-
-    override fun putAll(from: Map<out K, V>) {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-    }
-
-    override fun remove(key: K): V? {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-    }
+expect class WeakReference<T : Any> {
+    fun get(): T?
 }
