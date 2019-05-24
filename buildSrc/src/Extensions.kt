7a991e1a372e6d16f715e1878fc6cd5c97af3cae
Campbell Jones <git@serebit.com>
2019-05-23 23:12:14 -0400

    Update Ktor to 1.2.0

--- buildSrc/src/Extensions.kt
+++ buildSrc/src/Extensions.kt
@@ -23,7 +23,7 @@ fun RepositoryHandler.soywiz() = maven("https://dl.bintray.com/soywiz/soywiz")
 /** Versions of dependencies for type-safe consistency. */
 object Versions {
     const val COROUTINES = "1.2.1"
-    const val KTOR = "1.2.0-rc2"
+    const val KTOR = "1.2.0"
     const val SERIALIZATION = "0.11.0"
     const val LOGKAT = "0.4.5"
     const val KLOCK = "1.4.0"
