5957b5afece562d24e0149bc17d666528f717432
LegendOfLelouch <LegendOfLelouch@gmail.com>
2019-06-17 12:43:38 +0300

    Another cleanup to fix merge.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,8 @@
 import com.jfrog.bintray.gradle.BintrayExtension
-import com.serebit.strife.gradle.*
+import com.serebit.strife.gradle.jitpack
+import com.serebit.strife.gradle.kotlinx
+import com.serebit.strife.gradle.soywiz
 import org.gradle.jvm.tasks.Jar
-import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
     kotlin("multiplatform") version "1.3.31" apply false
@@ -9,22 +10,20 @@ plugins {
     id("org.jetbrains.dokka") version "0.9.18" apply false
 
     id("com.github.ben-manes.versions") version "0.21.0"
-    id("com.gradle.build-scan") version "2.2.1"
+    id("com.gradle.build-scan") version "2.3"
     id("com.jfrog.bintray") version "1.8.4"
     `maven-publish`
 }
 
 allprojects {
     group = "com.serebit"
-    version = "PEPE_SILVIA"
+    version = "0.1.0-SNAPSHOT"
 }
 
 subprojects {
     repositories {
         jcenter()
         kotlinx()
-        kotlinEap()
-        ktor()
         soywiz()
         jitpack()
     }
@@ -36,11 +35,6 @@ subprojects {
         tasks.withType<Jar> {
             // set jar base names to module paths, like strife-core and strife-samples-embeds
             archiveBaseName.set(fullPath)
-        }
-
-        tasks.withType<KotlinCompile> {
-            // configure experimental for coroutines channel API, along with ktor websockets
-            kotlinOptions.freeCompilerArgs = listOf("-progressive", "-Xuse-experimental=kotlin.Experimental")
         }
     }
 
@@ -65,7 +59,7 @@ buildScan {
 bintray {
     user = "serebit"
     System.getenv("BINTRAY_KEY")?.let { key = it }
-    System.getenv("BINTRAY_PUBLICATION")?.let { setPublications(it) }
+    setPublications(*publishing.publications.map { it.name }.toTypedArray())
     pkg(delegateClosureOf<BintrayExtension.PackageConfig> {
         repo = "public"
         name = rootProject.name
