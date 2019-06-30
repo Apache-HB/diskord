fb60d90be156ec80397b29bd25a845aa15a90b45
Campbell Jones <git@serebit.com>
2019-06-30 16:30:55 -0400

    Enable Gradle metadata publishing

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -2,6 +2,8 @@ rootProject.name = "strife"
 
 include(":client", ":samples:ping", ":samples:embeds")
 
+enableFeaturePreview("GRADLE_METADATA")
+
 pluginManagement.resolutionStrategy.eachPlugin {
     if (requested.id.id == "kotlinx-serialization")
         useModule("org.jetbrains.kotlin:kotlin-serialization:${requested.version}")
