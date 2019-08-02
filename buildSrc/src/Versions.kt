25f423ab6d28398e5c48add36d8927074df87481
Campbell Jones <git@serebit.com>
2019-08-01 22:44:16 -0400

    Update kotlinx.coroutines and ktor

--- buildSrc/src/Versions.kt
+++ buildSrc/src/Versions.kt
@@ -2,8 +2,8 @@ package com.serebit.strife.gradle
 
 /** Versions of dependencies for type-safe consistency. */
 object Versions {
-    const val COROUTINES = "1.3.0-M2"
-    const val KTOR = "1.2.2"
+    const val COROUTINES = "1.3.0-RC"
+    const val KTOR = "1.2.3"
     const val SERIALIZATION = "0.11.1"
     const val LOGKAT = "0.4.6"
     const val KLOCK = "1.5.0"
