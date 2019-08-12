461c2a06f6bd34129ac188400400255acaa75991
Campbell Jones <git@serebit.com>
2019-08-12 16:04:26 -0400

    Add documentation

--- client/src/commonMain/kotlin/data/Presence.kt
+++ client/src/commonMain/kotlin/data/Presence.kt
@@ -176,6 +176,7 @@ class Activity internal constructor(packet: ActivityPacket) {
         val match: String? = packet.match
     }
 
+    /** According to Discord, this describes what the payload includes. */
     enum class Flag(internal val value: Short) {
         INSTANCE(1 shl 0),
         JOIN(1 shl 1),
