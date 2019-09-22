051ee9c3c603d283b3147e68f86d320c7fb9e0b7
JonoAugustine <jaugust8@jhu.edu>
2019-09-22 19:02:23 -0400

    List.move extension and test

--- client/src/commonMain/kotlin/internal/Extensions.kt
+++ client/src/commonMain/kotlin/internal/Extensions.kt
@@ -4,3 +4,11 @@ import com.soywiz.klock.DateFormat
 
 /** [DateFormat] => yyyy-MM-dd'T'HH:mm:ss.SSSSSSXX */
 internal val DateFormat.Companion.ISO get() = DateFormat("yyyy-MM-dd'T'HH:mm:ss[.SSSSSS]XX").withOptional()
+
+internal inline fun <reified T : Any?> MutableList<T>.move(fromIndex: Int, toIndex: Int) =
+    apply { add(toIndex, removeAt(fromIndex)) }
+
+/** Apply the [mappin]g function to produce a new index from the current index and value. */
+internal inline fun <reified T : Any?> List<T>.move(mapping: (Int, T) -> Int): List<T> =
+    mapIndexed { index, t -> mapping(index, t) to t }.sortedBy { it.first }.map { it.second }
+
