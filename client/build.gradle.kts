415f4c4dad147639d0c9dcebaab651c62b6b0f4d
Campbell Jones <git@serebit.com>
2019-12-18 11:04:40 -0500

    Update Ktor to 1.3.0-rc. Fixes issue with attachment sending

--- client/build.gradle.kts
+++ client/build.gradle.kts
@@ -11,7 +11,7 @@ kotlin {
     sourceSets.commonMain.get().dependencies {
         implementation(kotlin("stdlib-common"))
         implementation(kotlinx("serialization-runtime-native", "0.14.0"))
-        implementation(ktor("client-core-native", "1.3.0-beta-2"))
+        implementation(ktor("client-core-native", "1.3.0-rc"))
         implementation("com.serebit.logkat", "logkat", "0.5.2")
         api(kotlinx("coroutines-core-native", "1.3.3"))
         api("com.soywiz.korlibs.klock", "klock", "1.8.1")
@@ -24,7 +24,7 @@ kotlin {
     jvm {
         compilations["main"].defaultSourceSet.dependencies {
             implementation(kotlin("stdlib-jdk8"))
-            implementation(ktor("client-cio", "1.3.0-beta-2"))
+            implementation(ktor("client-cio", "1.3.0-rc"))
         }
         compilations["test"].defaultSourceSet.dependencies {
             implementation(kotlin("test-junit5"))
