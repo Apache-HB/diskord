f40902dd61e18f5958d727800f0ac6de5d7b76cb
Campbell Jones <git@serebit.com>
2020-02-18 13:45:56 -0500

    Update Gradle wrapper and dependencies

--- client/build.gradle.kts
+++ client/build.gradle.kts
@@ -11,7 +11,7 @@ kotlin {
     sourceSets.commonMain.get().dependencies {
         implementation(kotlin("stdlib-common"))
         implementation(kotlinx("serialization-runtime-native", "0.14.0"))
-        implementation(ktor("client-core-native", "1.3.0"))
+        implementation(ktor("client-core-native", "1.3.1"))
         implementation("com.serebit.logkat", "logkat", "0.5.2")
         api(kotlinx("coroutines-core-native", "1.3.3"))
         api("com.soywiz.korlibs.klock", "klock", "1.8.6")
@@ -24,11 +24,11 @@ kotlin {
     jvm {
         compilations["main"].defaultSourceSet.dependencies {
             implementation(kotlin("stdlib-jdk8"))
-            implementation(ktor("client-cio", "1.3.0"))
+            implementation(ktor("client-cio", "1.3.1"))
         }
         compilations["test"].defaultSourceSet.dependencies {
             implementation(kotlin("test-junit5"))
-            implementation("org.junit.jupiter", "junit-jupiter", "5.5.2")
+            implementation("org.junit.jupiter", "junit-jupiter", "5.6.0")
         }
     }
 
